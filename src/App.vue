d594ae165c9b7259ee637f24f4176aa0be4eb342
gundasrikanth1 <srikanthgunda30@gmail.com>
2022-08-01 15:24:30 +0530

    added multi level dropdown changes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <MultiSelect />
+  </v-app>
+</template>
+
+<script>
+import MultiSelect from './components/MultiSelect.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    MultiSelect
+  }
+};
+</script>
---
+++ src/components/MultiSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="ma-auto" style="width: 500px">
+    <v-sheet class="pa-4 primary lighten-2">
+      <v-text-field
+        v-model="search"
+        label="Enter text here to search .."
+        dark
+        flat
+        solo-inverted
+        hide-details
+        clearable
+        clear-icon="mdi-close-circle-outline"
+      ></v-text-field>
+    </v-sheet>
+    <v-card-text>
+      <v-treeview :items="items" :search="search" :open.sync="open">
+      </v-treeview>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+import myJson from '../components/data.json';
+export default {
+  data: () => ({
+    items: myJson,
+    open: [],
+    search: null
+  })
+};
+</script>
